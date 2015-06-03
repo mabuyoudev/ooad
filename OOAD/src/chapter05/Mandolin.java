bb25d7fe882c3638c947260b8af7644fdfc893f5
yusuke mabuchi <mabu.you.dev@gmail.com>
2015-06-03 22:48:39 +0900

    第5章の途中

---
+++ OOAD/src/chapter05/Mandolin.java
@@ -0,0 +1,9 @@
+package chapter05;
+
+public class Mandolin extends Instrument {
+
+  public Mandolin(String serialNumber, double price, MandlinSpec spec) {
+    super(serialNumber, price, spec);
+  }
+
+}
